9ffcc89e05c16f5ea2efad2b1c7ed150a9bed1ce
Silvian <silvian.dragan@gmail.com>
2020-06-18 17:48:19 +0100

    added optional ability to customize sender id

--- lambda_function.py
+++ lambda_function.py
@@ -12,19 +12,43 @@ def handler(request, context):
     {
         "phone": "+44123456789",
         "country_code": "GB",
+        "sender_id": "Sender Name",
         "message": "Sample message text."
     }
     """
 
+    sns = boto3.client('sns')
+    phone = request.get('phone')
+    country_code = request.get('country_code')
+    sender_id = request.get('sender_id')
+    message = request.get('message')
+
+    if not phone:
+        return {"message": "Missing phone number."}
+
+    if not message:
+        return {"message": "Missing message body."}
+
     try:
-        sns = boto3.client('sns')
-        message = request.get('message')
-        phone_number = parse_phone_number(request.get('phone'), request.get('country_code'))
+        phone_number = parse_phone_number(phone, country_code)
     except (InvalidNumberException, ParsingException) as e:
         return {"message": str(e)}
-    
+
     print('Sending sms message: "{}" to: {}'.format(message, phone_number))
-    
-    sns.publish(PhoneNumber=phone_number, Message=message)
-    
+
+    message_attributes = {}
+    if sender_id:
+        message_attributes = {
+            'AWS.SNS.SMS.SenderID': {
+                'DataType': 'String',
+                'StringValue': sender_id,
+            }
+        }
+
+    sns.publish(
+        PhoneNumber=phone_number,
+        Message=message,
+        MessageAttributes=message_attributes,
+    )
+
     return {"success": True}
